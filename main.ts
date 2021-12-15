d12d129b78266a30e47be15424c415ca4e7917ba
TStilting <71121043+TStilting@users.noreply.github.com>
2021-12-15 10:15:08 +0100

    A now progresses the text box

--- main.ts
+++ main.ts
@@ -25,32 +25,16 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         functionA = 0
     }
     if (pointer.overlapsWith(ball)) {
-        pointer.destroy()
-        ball.destroy()
-        treat.destroy()
-        rock.destroy()
-        run.destroy()
+        removeButton()
         useBall()
     } else if (pointer.overlapsWith(treat)) {
-        pointer.destroy()
-        ball.destroy()
-        treat.destroy()
-        rock.destroy()
-        run.destroy()
+        removeButton()
         useTreat()
     } else if (pointer.overlapsWith(rock)) {
-        pointer.destroy()
-        ball.destroy()
-        treat.destroy()
-        rock.destroy()
-        run.destroy()
+        removeButton()
         useRock()
     } else if (pointer.overlapsWith(run)) {
-        pointer.destroy()
-        ball.destroy()
-        treat.destroy()
-        rock.destroy()
-        run.destroy()
+        removeButton()
         justRun()
     }
 })
@@ -58,118 +42,145 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 function useRock () {
     textSprite = textsprite.create("You used a rock", 1, 15)
     textSprite.setPosition(50, 100)
-    aggravation += 1
-    timer.after(2500, function () {
+    _throw = randint(0, 100)
+    while (!(controller.A.isPressed())) {
         textSprite.destroy()
-        HP.value = HP.value - randint(10, 20)
-        if (HP.value == 0) {
-            textSprite = textsprite.create("The opposing [enter name here] fainted", 1, 15)
-            textSprite.setPosition(50, 100)
-            timer.after(5000, function () {
-                textSprite.destroy()
+        if (_throw < 15) {
+            timer.after(4500, function () {
+                textSprite = textsprite.create("The Rock missed", 1, 15)
+                textSprite.setPosition(50, 100)
             })
+            while (!(controller.A.isPressed())) {
+                textSprite.destroy()
+                enemyTurn()
+            }
         } else {
-            enemyTurn()
+            aggravation += 1
+            HP.value = HP.value - randint(10, 20)
+            if (HP.value == 0) {
+                textSprite = textsprite.create("The opposing [enter name here] fainted", 1, 15)
+                textSprite.setPosition(50, 100)
+                creature.vy += 100
+                timer.after(100, function () {
+                    creature.destroy()
+                })
+                while (!(controller.A.isPressed())) {
+                    textSprite.destroy()
+                    timer.after(2000, function () {
+                        returnToMap()
+                    })
+                }
+            } else {
+                enemyTurn()
+            }
         }
-    })
+    }
 }
 function turnStart () {
     functionA = 1
     ball = sprites.create(img`
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        66666666666666666666666666666666
+        66666666666666666666666666666666
+        66666666666666666666666666666666
+        66666666666666666666666666666666
+        66666666666666666666666666666666
+        66666666666666666666666666666666
+        66666666666666666666666666666666
+        66666666666666666666666666666666
+        66666666666666666666666666666666
+        66666666666666666666666666666666
+        66666666666666666666666666666666
+        66666666666666666666666666666666
+        66666666666666666666666666666666
+        66666666666666666666666666666666
+        66666666666666666666666666666666
+        66666666666666666666666666666666
         `, SpriteKind.button)
-    rock = sprites.create(img`
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        b b b b b b b b b b b b b b b b 
-        `, SpriteKind.button)
-    run = sprites.create(img`
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        e e e e e e e e e e e e e e e e 
-        `, SpriteKind.button)
+    textBall = textsprite.create("Catch")
+    ball.setPosition(132, 53)
+    textBall.setPosition(132, 53)
     treat = sprites.create(img`
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-        7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+        77777777777777777777777777777777
+        77777777777777777777777777777777
+        77777777777777777777777777777777
+        77777777777777777777777777777777
+        77777777777777777777777777777777
+        77777777777777777777777777777777
+        77777777777777777777777777777777
+        77777777777777777777777777777777
+        77777777777777777777777777777777
+        77777777777777777777777777777777
+        77777777777777777777777777777777
+        77777777777777777777777777777777
+        77777777777777777777777777777777
+        77777777777777777777777777777777
+        77777777777777777777777777777777
+        77777777777777777777777777777777
         `, SpriteKind.button)
+    textTreat = textsprite.create("Treat")
+    treat.setPosition(132, 70)
+    textTreat.setPosition(132, 70)
+    rock = sprites.create(img`
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        `, SpriteKind.button)
+    textRock = textsprite.create("Rock")
+    rock.setPosition(132, 87)
+    textRock.setPosition(132, 87)
+    run = sprites.create(img`
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        `, SpriteKind.button)
+    textRun = textsprite.create("Run")
+    run.setPosition(132, 104)
+    textRun.setPosition(132, 104)
     pointer = sprites.create(img`
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-        5 5 . . . . . . . . . . . . 5 5 
-        5 . . . . . . . . . . . . . . 5 
-        5 . . . . . . . . . . . . . . 5 
-        5 . . . . . . . . . . . . . . 5 
-        5 . . . . . . . . . . . . . . 5 
-        5 . . . . . . . . . . . . . . 5 
-        5 . . . . . . . . . . . . . . 5 
-        5 . . . . . . . . . . . . . . 5 
-        5 . . . . . . . . . . . . . . 5 
-        5 . . . . . . . . . . . . . . 5 
-        5 . . . . . . . . . . . . . . 5 
-        5 . . . . . . . . . . . . . . 5 
-        5 . . . . . . . . . . . . . . 5 
-        5 5 . . . . . . . . . . . . 5 5 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+        55555555555555555555555555555555
+        55............................55
+        5.5..........................5.5
+        5..............................5
+        5..............................5
+        5..............................5
+        5..............................5
+        5..............................5
+        5..............................5
+        5..............................5
+        5..............................5
+        5..............................5
+        5..............................5
+        5.5..........................5.5
+        55............................55
+        55555555555555555555555555555555
         `, SpriteKind.cursor)
     pointer.setPosition(132, 53)
-    ball.setPosition(132, 53)
-    treat.setPosition(132, 70)
-    rock.setPosition(132, 87)
-    run.setPosition(132, 104)
 }
 // make it return to the map here
 function justRun () {
@@ -177,15 +188,17 @@ function justRun () {
     if (runChance < 5) {
         textSprite = textsprite.create("You couldn't get away", 1, 15)
         textSprite.setPosition(50, 100)
-        timer.after(2500, function () {
+        while (!(controller.A.isPressed())) {
+            textSprite.destroy()
             enemyTurn()
-        })
+        }
     } else {
         textSprite = textsprite.create("You got away savely", 1, 15)
         textSprite.setPosition(50, 100)
-        timer.after(2500, function () {
+        while (!(controller.A.isPressed())) {
             textSprite.destroy()
-        })
+            returnToMap()
+        }
     }
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -201,26 +214,34 @@ function enemyTurn () {
     timer.after(2500, function () {
         enemyChoise = aggravation * 10 + randint(0, 100)
         if (enemyChoise < 95) {
+            creature.vx += 100
+            timer.after(100, function () {
+                creature.destroy()
+            })
             textSprite = textsprite.create("The opposing [enter name here] fled", 1, 15)
             textSprite.setPosition(50, 100)
-            timer.after(2500, function () {
+            while (!(controller.A.isPressed())) {
                 textSprite.destroy()
-            })
+                returnToMap()
+            }
         } else {
             textSprite = textsprite.create("The opposing [enter name here] is watching carefully", 1, 15)
             textSprite.setPosition(50, 100)
-            timer.after(2500, function () {
+            while (!(controller.A.isPressed())) {
                 textSprite.destroy()
                 turnStart()
-            })
+            }
         }
     })
 }
+function returnToMap () {
+	
+}
 // makes the player throw the ball if selected
 function useBall () {
-    textSprite = textsprite.create("You used Ball", 1, 15)
+    textSprite = textsprite.create("You used the Net Stone", 1, 15)
     textSprite.setPosition(50, 100)
-    timer.after(2500, function () {
+    while (!(controller.A.isPressed())) {
         textSprite.destroy()
         catchRate = aggravation * 10 + (randint(0, 100) + HP.value)
         if (catchRate < 200) {
@@ -232,23 +253,39 @@ function useBall () {
                 textSprite.destroy()
             })
             timer.after(12500, function () {
-            	
+                returnToMap()
             })
         } else {
             timer.after(randint(7500, 10000), function () {
                 textSprite = textsprite.create("[enter name here] broke free", 1, 15)
                 textSprite.setPosition(50, 100)
             })
-            timer.after(12500, function () {
+            while (!(controller.A.isPressed())) {
                 textSprite.destroy()
                 enemyTurn()
-            })
+            }
         }
-    })
+    }
+}
+function removeButton () {
+    pointer.destroy()
+    ball.destroy()
+    treat.destroy()
+    rock.destroy()
+    run.destroy()
+    textBall.destroy()
+    textTreat.destroy()
+    textRock.destroy()
+    textRun.destroy()
 }
 let catchRate = 0
 let enemyChoise = 0
 let runChance = 0
+let textRun: TextSprite = null
+let textRock: TextSprite = null
+let textTreat: TextSprite = null
+let textBall: TextSprite = null
+let _throw = 0
 let run: Sprite = null
 let rock: Sprite = null
 let treat: Sprite = null
@@ -258,6 +295,45 @@ let functionA = 0
 let HP: StatusBarSprite = null
 let textSprite: TextSprite = null
 let aggravation = 0
+let creature: Sprite = null
+let character = sprites.create(img`
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a f a a a a a a f a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a f a a a a a a a a a f a a 
+    a a a a f f f f f f f f f a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    `, SpriteKind.Player)
+character.setPosition(25, 69)
+creature = sprites.create(img`
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    `, SpriteKind.Enemy)
+creature.setPosition(104, 35)
 aggravation = 5
 textSprite = textsprite.create("", 1, 15)
 textSprite.setPosition(50, 100)
